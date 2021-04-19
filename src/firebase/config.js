a7bfc42e359bfdcd425bf7472510f0808545eb33
sameep-s <36707002+sameep-s@users.noreply.github.com>
2021-04-20 01:39:45 +0530

    Key Update.js

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -4,12 +4,7 @@ import 'firebase/firestore';
 
 // Your web app's Firebase configuration
 var firebaseConfig = {
-  apiKey: "AIzaSyCg-slXUOBMmaZE1vvMVXxqCzE4Y0_hyWo",
-  authDomain: "firegram-297ca.firebaseapp.com",
-  projectId: "firegram-297ca",
-  storageBucket: "firegram-297ca.appspot.com",
-  messagingSenderId: "377063014271",
-  appId: "1:377063014271:web:6fb52d215b5789098a244a"
+//Enter you key here!
 };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
